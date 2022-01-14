7c5d93acff39ea5a76faf69d6b135460c5d7ea80
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-01-14 10:59:08 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from .dic import *
+import re
+
+def encrypt(text:str):
+    return ''.join(encrypt_dict[c] for c in list(text))
+
+def decrypt(cipher:str):
+    # \d{2} matches 2 digits, \w matches letters, \d\s matches a digit followed by 1 whitespace, \s+ matches 1 or more whitespaces, \d$ matches a single digit at the end of the string
+    split_num = re.findall("\d{2}|\w|\d\s|\s+|\d$", cipher)
+    decrypted = ''.join(decrypt_dict[c] for c in split_num)
+    return decrypted
