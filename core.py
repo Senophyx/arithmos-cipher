59e98b57d294e46d6844ecc4f597665f0e0021a3
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-01-15 12:52:34 +0700

    Update core.py

--- core.py
+++ core.py
@@ -7,6 +7,6 @@ def encrypt(text:str):
 def decrypt(cipher:int):
     cipher = str(cipher)
     # \d{2} matches 2 digits, \w matches letters, \d\s matches a digit followed by 1 whitespace, \s+ matches 1 or more whitespaces, \d$ matches a single digit at the end of the string
-    split_num = re.findall("\d{2}|\w|\d\s|\s+|\d$", cipher)
+    split_num = re.findall("\d{2}|\w|.|\d\s|\s+|\d$", cipher)
     decrypted = ''.join(decrypt_dict[c] for c in split_num)
     return decrypted
