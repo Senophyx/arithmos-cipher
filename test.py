d646e9b4cb735c8a8cf4c6e08671d45d79a19954
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-03-06 18:29:07 +0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+from core import *
+
+word = "Testing 123 @#=/@)"
+
+# Encrypt
+encrypted = encrypt(word)
+print("==============================")
+print(encrypted)
+
+# Decrypt
+decrypted = decrypt(encrypted)
+print(decrypted)
+print("==============================")
