88063e3b02f55abd761ec84774b5f4b190488ad8
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-03-19 12:50:20 +0700

    Rename `req_version` to `get_version`

--- arithmos/utils.py
+++ arithmos/utils.py
@@ -10,7 +10,7 @@ except ImportError:
 URL_PATTERN = 'https://pypi.python.org/pypi/{package}/json'
 
 
-def req_version(package="arithmos-cipher", url_pattern=URL_PATTERN):
+def get_version(package="arithmos-cipher", url_pattern=URL_PATTERN):
     """Return version of package on pypi.python.org using json."""
     try:
         req = requests.get(url_pattern.format(package=package))
