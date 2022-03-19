365c36cebb828c810f76b64c97a309fe7c3e158d
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-03-19 12:25:50 +0700

    Update. if offline, pass

--- arithmos/utils.py
+++ arithmos/utils.py
@@ -1,4 +1,5 @@
 import requests
+from requests import RequestException
 import json
 try:
     from packaging.version import parse
@@ -11,13 +12,17 @@ URL_PATTERN = 'https://pypi.python.org/pypi/{package}/json'
 
 def get_version(package="arithmos-cipher", url_pattern=URL_PATTERN):
     """Return version of package on pypi.python.org using json."""
-    req = requests.get(url_pattern.format(package=package))
-    version = parse('0')
-    if req.status_code == requests.codes.ok:
-        j = json.loads(req.text.encode(req.encoding))
-        releases = j.get('releases', [])
-        for release in releases:
-            ver = parse(release)
-            if not ver.is_prerelease:
-                version = max(version, ver)
-    return version
+    try:
+        req = requests.get(url_pattern.format(package=package))
+        version = parse('0')
+        if req.status_code == requests.codes.ok:
+            j = json.loads(req.text.encode(req.encoding))
+            releases = j.get('releases', [])
+            for release in releases:
+                ver = parse(release)
+                if not ver.is_prerelease:
+                    version = max(version, ver)
+        return version
+        
+    except RequestException:
+        pass
