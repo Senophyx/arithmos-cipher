2ce12a9a945375ed90cd4f41bf98769dad61a4f4
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-03-19 11:58:30 +0700

    Create utils.py

---
+++ arithmos/utils.py
@@ -0,0 +1,23 @@
+import requests
+import json
+try:
+    from packaging.version import parse
+except ImportError:
+    from pip._vendor.packaging.version import parse
+
+
+URL_PATTERN = 'https://pypi.python.org/pypi/{package}/json'
+
+
+def get_version(package="arithmos-cipher", url_pattern=URL_PATTERN):
+    """Return version of package on pypi.python.org using json."""
+    req = requests.get(url_pattern.format(package=package))
+    version = parse('0')
+    if req.status_code == requests.codes.ok:
+        j = json.loads(req.text.encode(req.encoding))
+        releases = j.get('releases', [])
+        for release in releases:
+            ver = parse(release)
+            if not ver.is_prerelease:
+                version = max(version, ver)
+    return version
