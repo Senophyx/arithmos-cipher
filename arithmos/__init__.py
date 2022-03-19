97eebfd7f6c8d3a459a9b1ae2458d47be9141b8d
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-03-19 13:04:33 +0700

    update version to `2.0`

--- arithmos/__init__.py
+++ arithmos/__init__.py
@@ -1,20 +1,8 @@
 from .encrypt import encrypt
 from .decrypt import decrypt
-from .utils import get_version
 
 __title__ = "Arithmos Cipher"
 __version__ = "2.0"
 __authors__ = "LyQuid"
 __license__ = "Apache License 2.0"
 __copyright__ = "Copyright 2022-present LyQuid"
-
-
-get_version = get_version()
-
-if get_version == "offline":
-    pass
-else:
-	if float(__version__) == float(str(get_version)):
-		pass
-	else:
-		print(f"Warning! Your arithmos-cipher version is outdated! Latest version : {float(str(get_version))}\n")
