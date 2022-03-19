e4769fadcd963f1567e4dd700e2ecc4882e9d1ea
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-03-19 17:20:15 +0700

    Changing `click.echo` to `print`

--- arithmos/cli.py
+++ arithmos/cli.py
@@ -16,7 +16,7 @@ def encrypt(text):
     for t in text:
         enc = ''.join(encrypt_dict[c] for c in list(t))
         encrypted.append(enc)
-    click.echo(" ".join(str(i) for i in encrypted))
+    print(" ".join(str(i) for i in encrypted))
 
 @click.command()
 @click.argument('cipher', nargs=-1)
@@ -26,11 +26,11 @@ def decrypt(cipher):
         decrypted.append(ciph)
     cipher = " ".join(str(i) for i in decrypted)
     separate = re.findall("\d{2}|\w|.|\d\s|\s+|\d$", cipher)
-    click.echo(''.join(decrypt_dict[c] for c in separate))
+    print(''.join(decrypt_dict[c] for c in separate))
 
 @click.command()
 def version():
-    click.echo(__version__)
+    print(__version__)
 
 cli.add_command(encrypt)
 cli.add_command(decrypt)
