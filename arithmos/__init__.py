adeb790b8ad7145a0edc9c56908f2e10da9a56e1
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-03-19 12:41:13 +0700

    Update __init__.py

--- arithmos/__init__.py
+++ arithmos/__init__.py
@@ -9,8 +9,12 @@ __license__ = "Apache License 2.0"
 __copyright__ = "Copyright 2022-present LyQuid"
 
 
+get_version = get_version()
 
-if float(__version__) == float(str(get_version())):
-	pass
+if get_version == "offline":
+    pass
 else:
-	print(f"Warning! Your arithmos-cipher version is outdated! Latest version : {get_version()}")
+    if float(__version__) == float(str(get_version())):
+	    pass
+	else:
+	    print(f"Warning! Your arithmos-cipher version is outdated! Latest version : {get_version()}")
