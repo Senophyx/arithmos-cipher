c13b500a1444d033264b1b7533130935b08846fd
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-03-21 17:29:12 +0700

    Update docstring

--- arithmos/encrypt.py
+++ arithmos/encrypt.py
@@ -2,7 +2,8 @@ from .dictionary import encrypt_dict
 
 def encrypt(text:str):
     """
-    A method to encrypt string to Arithmos
+    A method to encrypt string to cipher.
+    Return to encrypted cipher.
     ----
     Parameter :
     - text: `str` | the string to be encrypted
