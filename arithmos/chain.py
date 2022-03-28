aabb4f716ece7fc982e14f9b85e683dad1010257
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-03-28 11:12:09 +0700

    Update chain function

--- arithmos/chain.py
+++ arithmos/chain.py
@@ -1,9 +1,20 @@
+from .encrypt import encrypt as EncFunc
+from .decrypt import decrypt as DecFunc
+
 class chain:
     def __init__(self, level:int):
         self.level = level
         
-    def encrypt(self):
-        pass
+    def encrypt(self, text:str):
+        level = self.level
+        enc = EncFunc(text)
+        for _ in range(level):
+            enc = EncFunc(enc)
+        return enc
     
     def decrypt(self):
-        pass
+        level = self.level
+        dec = DecFunc(text)
+        for _ in range(level):
+            dec = DecFunc(dec)
+        return dec
