09fd3df88a49d7cff239a9ef9a7911302001fae0
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-03-28 14:22:35 +0700

    Added docstring and `@classmethod`

--- arithmos/chain.py
+++ arithmos/chain.py
@@ -2,17 +2,42 @@ from .encrypt import encrypt as EncFunc
 from .decrypt import decrypt as DecFunc
 
 class chain:
+    """
+    A class to chaining arithmos for more security.
+    ----
+    Parameter :
+    - level: `int` | how many times must arithmos be encrypted (chaining).
+    Classmethod :
+    - encrypt
+    - decrypt
+    
+    No return.
+    """
     def __init__(self, level:int):
         self.level = level
-        
+      
+    @classmethod  
     def encrypt(self, text:str):
+        """
+        A method to encrypt text repeatedly (based on level).
+        ----
+        Parameter:
+        - text: `str`
+        """
         level = self.level
         enc = EncFunc(text)
         for _ in range(level):
             enc = EncFunc(enc)
         return enc
     
+    @classmethod
     def decrypt(self, cipher:str):
+        """
+        A method to decrypt text repeatedly (based on level) until find the actual text.
+        ----
+        Parameter:
+        - cipher: `str`
+        """
         cipher = str(cipher)
         level = self.level
         dec = DecFunc(cipher)
