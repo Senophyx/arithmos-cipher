edd41f38784095e13029dd0e270ca6eb12bdb9fe
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-04-01 10:14:39 +0700

    Added `UnknownKey` error

---
+++ arithmos/errors.py
@@ -0,0 +1,9 @@
+class ArithmosErrors(Exception):
+    def __init__(self, message:str=None):
+        if message is None:
+            message = 'An unknown error has occurred within Arithmos program.'
+        super().__init__(message)
+        
+class UnknownKey(ArithmosErrors):
+    def __init__(self, key:str):
+        super().__init__(f"{key} cannot be encrypted.")
