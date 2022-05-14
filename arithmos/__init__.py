b5044ff51cf1c3c9607324cefee19a01b519fa90
LyQuid :3 <83483788+LyQuid12@users.noreply.github.com>
2022-05-14 11:33:29 +0700

    update `__version__` to `3.5`

--- arithmos/__init__.py
+++ arithmos/__init__.py
@@ -3,7 +3,7 @@ from .decrypt import decrypt
 from .chain import chain
 
 __title__ = "Arithmos Cipher"
-__version__ = "3.5b3"
+__version__ = "3.5"
 __authors__ = "LyQuid"
 __license__ = "Apache License 2.0"
 __copyright__ = "Copyright 2022-present LyQuid"
